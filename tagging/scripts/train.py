61d2a9d6c221b16baa73e33935116a7753444824
Pablo Gómez <pagomez@despegar.com>
2018-02-27 19:21:28 -0300

    Added flow on train.py file to train an memm model

--- tagging/scripts/train.py
+++ tagging/scripts/train.py
@@ -23,13 +23,13 @@ import pickle
 from ancora import SimpleAncoraCorpusReader
 
 from tagging.baseline import BaselineTagger, BadBaselineTagger
-# from tagging.memm import MEMM
+from tagging.memm import MEMM
 
 
 models = {
     'badbase': BadBaselineTagger,
     'base': BaselineTagger,
-    # 'memm': MEMM,
+    'memm': MEMM,
 }
 
 
@@ -43,19 +43,19 @@ if __name__ == '__main__':
 
     # train the model
     model_class = models[opts['-m']]
-    model = model_class(sents)
+    # model = model_class(sents)
 
     # USEFUL FOR MODELS WITH PARAMETERS:
-    # if opts['-n']:
-    #     n = int(opts['-n'])
-    #     if opts['-m'] == 'memm':
-    #         clf = opts['-c']
-    #         model = model_class(n, sents, clf=clf)
-    #     else:
-    #         model = model_class(n, sents)
-    # else:
-    #     # only for baselines
-    #     model = model_class(sents)
+    if opts['-n']:
+        n = int(opts['-n'])
+        if opts['-m'] == 'memm':
+            clf = opts['-c']
+            model = model_class(n, sents, clf=clf)
+        else:
+            model = model_class(n, sents)
+    else:
+        # only for baselines
+        model = model_class(sents)
 
     # save it
     filename = opts['-o']
